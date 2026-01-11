059628b64355067a0b868272043c2a936cc608e5
Abizer Masavi <133958757+abizer007@users.noreply.github.com>
2026-01-11 16:00:32 +0530

    v3

--- seekr-talent-hub-main/src/main.tsx
+++ seekr-talent-hub-main/src/main.tsx
@@ -1,13 +1,8 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
-
-createRoot(document.getElementById("root")!).render(<App />);
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
